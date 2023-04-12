0a2a47278c5e67c796e150f1db42378f28de2fac
marufahmedofficial <mk6116055@gmail.com>
2023-04-12 22:39:08 +0600

    initial commits

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Movie List App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
