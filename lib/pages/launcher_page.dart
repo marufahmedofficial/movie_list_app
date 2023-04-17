60562b83e4744819f3000afc24eb05c7c6ed5f52
marufahmedofficial <mk6116055@gmail.com>
2023-04-17 13:54:37 +0600

    launcher page

---
+++ lib/pages/launcher_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/user_provider.dart';
+import '../utils/helper_functions.dart';
+import 'movie_list.dart';
+class LauncherPage extends StatefulWidget {
+  static const String routeName = '/launcher';
+  const LauncherPage({Key? key}) : super(key: key);
+
+  @override
+  State<LauncherPage> createState() => _LauncherPageState();
+}
+
+class _LauncherPageState extends State<LauncherPage> {
+  @override
+  void initState() {
+    redirectUser();
+    super.initState();
+  }
+
+  Future<void> redirectUser() async {
+    if(await getLoginStatus()) {
+      final id = await getUserId();
+      await Provider.of<UserProvider>(context, listen: false).getUserById(id);
+      Navigator.pushReplacementNamed(context, MovieListPage.routeName);
+    } else {
+      Navigator.pushReplacementNamed(context, LoginPage.routeName);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator(),),
+    );
+  }
+}
